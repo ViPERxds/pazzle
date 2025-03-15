e02bfc58a2d53656c8ed11f0a87f5a1a0ff1041f
DraveTech <166045125+ViPERxds@users.noreply.github.com>
2025-03-15 14:23:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -830,14 +830,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 success: success,
                 time: elapsedTime,
                 userRating: {
-                    rating: newUserRatingData.rating.toFixed(8),
-                    rd: newUserRatingData.rd.toFixed(8),
-                    volatility: newUserRatingData.volatility.toFixed(8)
+                    rating: parseFloat(newUserRatingData.rating).toFixed(8),
+                    rd: parseFloat(newUserRatingData.rd).toFixed(8),
+                    volatility: parseFloat(newUserRatingData.volatility).toFixed(8)
                 },
                 puzzleRating: {
-                    rating: newPuzzleRatingData.rating.toFixed(8),
-                    rd: newPuzzleRatingData.rd.toFixed(8),
-                    volatility: newPuzzleRatingData.volatility.toFixed(8)
+                    rating: parseFloat(newPuzzleRatingData.rating).toFixed(8),
+                    rd: parseFloat(newPuzzleRatingData.rd).toFixed(8),
+                    volatility: parseFloat(newPuzzleRatingData.volatility).toFixed(8)
                 }
             };
             
